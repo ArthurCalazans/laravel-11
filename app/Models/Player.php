aa0334f0a328a94ea53687deeb42a20b89471e2e
Arthur Calazans <arthurcalazns1@gmail.com>
2024-05-04 13:52:38 -0300

    criando o jogador

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'nickname'];
+
+    public function characters()
+    {
+        return $this->hasMany(RpgCharacter::class);
+    }
 }

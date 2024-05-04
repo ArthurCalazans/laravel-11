a669d35729f01e77720b95dfd46ba7d5a4db619c
Arthur Calazans <arthurcalazns1@gmail.com>
2024-05-04 13:49:28 -0300

    Estrutura basica de banco

---
+++ app/Models/RpgCampaign.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RpgCampaign extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/RpgSpell.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RpgSpell extends Model
+{
+    use HasFactory;
+}
